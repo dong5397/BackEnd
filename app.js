bf3a97f59d73066b3c0f802eaba6f35d594f11b4
dong5397 <dongwok5397@naver.com>
2023-12-29 14:58:50 +0900

    init

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 import express from "express";
+
 const app = express();
 const port = 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const wiseSayings = [
+  {
+    content: "나는 김동욱이다",
+    author: "김동욱",
+  },
+  { content: "나는 김동욱2이다", author: "김동욱2" },
+];
+app.get("/wise-sayings", (req, res) => {
+  res.json(wiseSayings);
 });
 
 app.listen(port, () => {
